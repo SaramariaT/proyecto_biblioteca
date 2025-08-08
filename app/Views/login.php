e4810d5eab08c449b95d56b9f53b2552aeaffb16
SaramariaT <20150026@colegioscj.edu.gt>
2025-08-08 10:57:08 -0600

    Integración del Login

---
+++ app/Views/login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login - Biblioteca</title>
+</head>
+<body>
+    <form action = "#" method = "post">
+        <label>Usuario:</label><br>
+        <input type = "text" name = "usuario"><br>
+        <label>Contraseña:</label><br>
+        <input type = "password" name = "password"><br><br>
+        <button type = "submit">Ingresar</button>
+    </form>
+</body>
+</html>
